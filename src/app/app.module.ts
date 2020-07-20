5c8b9fe42194ec412d4c04eaaf5bfca72839c133
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-07-20 16:07:29 -0400

    Advanced Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,35 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MatCardModule } from '@angular/material/card';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    QuestionFormComponent,
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
